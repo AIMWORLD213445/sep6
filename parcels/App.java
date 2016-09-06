a9d978364926de471ffda14c548c6aee4926696b
Andrew Merrell and Brian Dutz <andrew@>
2016-09-06 14:56:50 -0700

    add gift wrap method and while loop to continue running program if user chooses to.

--- parcels/App.java
+++ parcels/App.java
@@ -5,23 +5,44 @@ import java.util.ArrayList;
 public class App{
   public static void main(String[] args){
     Console myConsole = System.console();
+    boolean programRunning = true;
 
-    System.out.println("Enter the height of your parcel (in inches).");
-    int userParcelHeight = Integer.parseInt(myConsole.readLine());
-    System.out.println("Enter the width of your parcel (in inches).");
-    int userParcelWidth = Integer.parseInt(myConsole.readLine());
-    System.out.println("Enter the length of your parcel (in inches).");
-    int userParcelLength = Integer.parseInt(myConsole.readLine());
-    System.out.println("Enter the weight of your parcel (in pounds).");
-    int userParcelWeight = Integer.parseInt(myConsole.readLine());
-    System.out.println("How far are you shipping your parcel (in miles)?");
-    int userParcelDistance = Integer.parseInt(myConsole.readLine());
-    System.out.println("How would you like to ship your parcel? Enter either 'standard', 'expedited', or 'overnight'.");
-    String userParcelDelivery = myConsole.readLine();
+    while(programRunning) {
+      System.out.println("Welcome to Parcel Ship. Choose 'calculate shipping' or 'exit'");
+      String userExit = myConsole.readLine();
 
-    Parcel userParcel = new Parcel(userParcelHeight, userParcelWidth, userParcelLength, userParcelWeight, userParcelDelivery, userParcelDistance);
-    userParcel.calcVolume();
-    userParcel.costToShip();
-    System.out.println("Your shipping cost is: $" + userParcel.mPrice);
+      if (userExit.equals("calculate shipping")) {
+        System.out.println("Enter the height of your parcel (in inches).");
+        int userParcelHeight = Integer.parseInt(myConsole.readLine());
+        System.out.println("Enter the width of your parcel (in inches).");
+        int userParcelWidth = Integer.parseInt(myConsole.readLine());
+        System.out.println("Enter the length of your parcel (in inches).");
+        int userParcelLength = Integer.parseInt(myConsole.readLine());
+        System.out.println("Enter the weight of your parcel (in pounds).");
+        int userParcelWeight = Integer.parseInt(myConsole.readLine());
+        System.out.println("How far are you shipping your parcel (in miles)?");
+        int userParcelDistance = Integer.parseInt(myConsole.readLine());
+        System.out.println("How would you like to ship your parcel? Enter either 'standard', 'expedited', or 'overnight'.");
+        String userParcelDelivery = myConsole.readLine();
+
+
+        Parcel userParcel = new Parcel(userParcelHeight, userParcelWidth, userParcelLength, userParcelWeight, userParcelDelivery, userParcelDistance);
+        userParcel.calcVolume();
+        userParcel.costToShip();
+        System.out.println("Your shipping cost is: $" + userParcel.mPrice);
+        System.out.println("Would you like gift wrapping? Enter either 'Yes' or 'No'");
+        String userParcelSurface = myConsole.readLine();
+        if (userParcelSurface.equals("Yes")){
+          userParcel.calcSurface();
+          System.out.println("Your total cost is: $" + userParcel.mPrice);
+        } else{
+          System.out.println("Your total cost is: $" + userParcel.mPrice);
+        }
+      } else if (userExit.equals("exit")) {
+        programRunning = false;
+      } else {
+        System.out.println("I'm sorry, we don't recognize your input");
+      }
+    }
   }
 }
--- parcels/Parcel.java
+++ parcels/Parcel.java
@@ -7,6 +7,7 @@ class Parcel {
   public String mDelivery;
   public int mDistance;
   public int mPrice;
+  public int mSurface;
 
   public Parcel(int height, int width, int length, int weight, String delivery, int distance) {
     mHeight = height;
@@ -23,13 +24,17 @@ class Parcel {
     mVolume = mHeight * mWidth * mLength;
   }
 
+  public void calcSurface() {
+    mPrice += ((2 * mHeight * mWidth) + (2 * mWidth * mLength) + (2 * mHeight * mLength))/ 100;
+  }
+
   public void costToShip() {
     if (mDelivery.equals("expedited")) {
-      mPrice = 25 + (mVolume / 4) + (mWeight * (mDistance / 1000));
+      mPrice = 25 + (mVolume / 40) + (mWeight * (mDistance / 1000));
     } else if (mDelivery.equals("standard")) {
-      mPrice = (mVolume / 4) + (mWeight * (mDistance / 1000)) + 5;
+      mPrice = (mVolume / 40) + (mWeight * (mDistance / 1000)) + 5;
     } else if (mDelivery.equals("overnight")) {
-      mPrice = (mVolume / 4 ) + (mWeight * (mDistance / 1000)) + 30;
+      mPrice = (mVolume / 40) + (mWeight * (mDistance / 1000)) + 30;
     } else {
       System.out.println("Sorry, we didn't get that request. Please choose 'standard', 'expedited', or 'overnight'");
     }
